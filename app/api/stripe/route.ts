21067466d5e61eee5433bfe4fd112d950f050715
Dan <daniel.speranskiy@live.com>
2023-11-09 18:08:22 +0400

    Integrating stripe for payments

---
+++ app/api/stripe/route.ts
@@ -0,0 +1,68 @@
+import { NextResponse } from 'next/server';
+import {auth, currentUser} from '@clerk/nextjs'
+import prismadb from '@/lib/prismadb';
+import {stripe} from '@/lib/stripe'
+import { absoluteUrl } from '@/lib/utils';
+
+const settingsUrl = absoluteUrl('/settings');
+
+
+export async function GET(){
+    try {
+        const {userId} = auth()
+        const user = await currentUser()
+
+        if(!userId || !user){
+            return new NextResponse('Unauthorized', { status: 401 })
+        }
+
+        const userSubscription = await prismadb.userSubscription.findUnique({
+            where: {
+                userId
+            }
+        })
+
+        if(userSubscription && userSubscription.stripeCustomerId){
+            const stripeSession = await stripe.billingPortal.sessions.create({
+                customer: userSubscription.stripeCustomerId,
+                return_url: settingsUrl
+            })
+           return new NextResponse(JSON.stringify({url: stripeSession.url}))
+        }
+
+        const stripeSession = await stripe.checkout.sessions.create({
+            success_url: settingsUrl,
+            cancel_url: settingsUrl,
+            payment_method_types: ['card'],
+            mode: 'subscription',
+            billing_address_collection: 'auto',
+            customer_email: user.emailAddresses[0].emailAddress,
+            line_items: [
+                {
+                    price_data: {
+                        currency: 'usd',
+                        product_data: {
+                            name: 'Personify AI Subscription',
+                            description: '1 month subscription to Personify AI to create your own AI Personify'
+                        },
+                        unit_amount: 1499,
+                        recurring:{
+                            interval: 'month',
+                            
+                        }
+                    },
+                    quantity: 1
+                }
+            ],
+            metadata: {
+                userId
+            }
+        })
+
+        return new NextResponse(JSON.stringify({url: stripeSession.url}))
+
+    } catch (error) {
+        console.log("STRIPE_ERROR",error)
+        return new NextResponse('Internal Server Error', { status: 500 })
+    }
+}
