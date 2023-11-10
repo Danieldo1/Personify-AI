33e7ba30f75c5ae7e73fa20332ebff1ca3d77289
Dan <daniel.speranskiy@live.com>
2023-11-10 12:21:32 +0400

    Fixing auth

--- app/api/chat/[chatId]/route.ts
+++ app/api/chat/[chatId]/route.ts
@@ -19,7 +19,7 @@ export async function POST(
     const { prompt } = await request.json();
     const user = await currentUser();
 
-    if (!user || !user.firstName || !user.id) {
+    if (!user  || !user.id) {
       return new NextResponse("Unauthorized", { status: 401 });
     }
 
